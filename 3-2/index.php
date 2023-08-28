d3c10e40e1d00f3e9dd7688ffcf8f32ac0337923
鎗田祐生 <70569608+Yuki-Yarita@users.noreply.github.com>
2023-08-28 18:13:29 +0900

    課題提出
    3-2

---
+++ 3-2/index.php
@@ -0,0 +1,22 @@
+<?php
+  // 商品の税込価格を計算しましょう
+  // ① 税率を定数TAXで作成しましょう。消費税は10%とします。
+  $tax = 0.1;
+
+  // ② 商品の情報を連想配列に入れましょう。
+  $products = ["鉛筆" => 100, "消しゴム" => 150, "物差し" => 500];
+
+  // ③ 税率を計算する関数を用意します。
+  // 引数には値段を受け取り、税込価格を返答します。
+  function priceTax($price) {
+    global $tax;
+    $zeikomi = $price + ($price * $tax);
+    return $zeikomi;
+  }
+
+  // ④ 繰り返し文を使って画面に指定の文字を表示しましょう！
+  foreach ($products as $product => $price) {
+    $taxIncluded = priceTax($price);
+    echo "{$product}の税込価格は{$taxIncluded}円です。<br>";
+  }
+?>
